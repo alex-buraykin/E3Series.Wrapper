bae76ee0ab7755d7bcbfcad0abd459557c6b0f84
Алексей Бурайкин <alex.buraykin@gmail.com>
2016-10-26 11:18:48 +0300

    (#1) Now its possible to connect to or disconnect from E3.series COM

--- E3Series.Wrapper.Demo/ViewModels/MainViewModel.cs
+++ E3Series.Wrapper.Demo/ViewModels/MainViewModel.cs
@@ -1,14 +1,54 @@
-﻿using E3Series.Wrapper.Demo.Views;
+﻿using System.Windows.Input;
+using E3Series.Wrapper.Demo.Views;
+using E3Series.Wrapper.Entities.Interfaces;
+using E3Series.Wrapper.Interfaces;
+using E3Series.Wrapper.SelectionDialog.WPF;
+using E3Series.Wrapper.SelectionDialog.WPF.Commands;
 using E3Series.Wrapper.SelectionDialog.WPF.ViewModels.Base;
 
 namespace E3Series.Wrapper.Demo.ViewModels
 {
     public class MainViewModel : ViewModelBase<MainWindow>
     {
+        #region Private Fields
+
+        private readonly IConnector _connector = new WpfConnector();
+        private ICommand _connectCommand;
+        private IApplication _app;
+        private ICommand _disconnectCommand;
+
+        #endregion
+
         #region Constructor
 
         public MainViewModel() : base(new MainWindow())
         {
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand ConnectCommand
+        {
+            get
+            {
+                return _connectCommand ??
+                       (_connectCommand = new RelayCommand(() => { _app = _connector.Connect(); }, () => _app == null));
+            }
+        }
+
+        public ICommand DisconnectCommand
+        {
+            get
+            {
+                return _disconnectCommand ??
+                       (_disconnectCommand = new RelayCommand(() =>
+                       {
+                           _app.Dispose();
+                           _app = null;
+                       }, () => _app != null));
+            }
         }
 
         #endregion
